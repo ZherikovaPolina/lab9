b11974cea9ab108ccd3869b6dc6d318b0c2fe186
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-25 23:31:00 +0300

    add more filer structure: add 2 different input from and to user will go to filter both distanation and add input for train's number

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,33 @@
-export default function SearchBar({ search, setSearch }) {
+export default function SearchBar({
+  from,
+  to,
+  number,
+  setFrom,
+  setTo,
+  setNumber,
+}) {
   return (
-    <input
-      className="search-input"
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      placeholder="Search trains..."
-    />
+    <div className="search-panel">
+      <input
+        className="search-input"
+        placeholder="Звідки"
+        value={from}
+        onChange={(e) => setFrom(e.target.value)}
+      />
+
+      <input
+        className="search-input"
+        placeholder="Куди"
+        value={to}
+        onChange={(e) => setTo(e.target.value)}
+      />
+
+      <input
+        className="search-input"
+        placeholder="Номер потяга"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+    </div>
   );
 }
