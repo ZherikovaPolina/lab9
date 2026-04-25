e70d3875d8356ee787758f00f43875fabcfb3a22
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-25 23:33:56 +0300

    add more filter details from SearchBar make project more useful and interesting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,30 @@ import TrainList from "../components/TrainList";
 import SearchBar from "../components/SearchBar";
 
 export default function Home() {
-  const [search, setSearch] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [number, setNumber] = useState("");
 
-  const filtered = trains.filter((t) =>
-    `${t.number} ${t.from} ${t.to}`
-      .toLowerCase()
-      .includes(search.toLowerCase())
-  );
+  const filtered = trains.filter((t) => {
+    return (
+      t.from.toLowerCase().includes(from.toLowerCase()) &&
+      t.to.toLowerCase().includes(to.toLowerCase()) &&
+      t.number.toLowerCase().includes(number.toLowerCase())
+    );
+  });
 
   return (
     <div className="container">
-      <h1>🚆 Railway Booking</h1>
+      <h1>Railway Booking</h1>
 
-      <SearchBar search={search} setSearch={setSearch} />
+      <SearchBar
+        from={from}
+        to={to}
+        number={number}
+        setFrom={setFrom}
+        setTo={setTo}
+        setNumber={setNumber}
+      />
 
       <TrainList trains={filtered} />
     </div>
