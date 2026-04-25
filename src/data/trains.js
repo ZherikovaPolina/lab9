e3526de76bfc60038f2737fc13135bf674678e43
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-25 23:04:24 +0300

    extend train data with departure time and duration and add more ways

--- src/data/trains.js
+++ src/data/trains.js
@@ -1,8 +1,26 @@
 export const trains = [
   {
     id: 1,
-    number: "743",
-    from: "Lviv",
-    to: "Kyiv"
+    number: "IC 743",
+    from: "Львів",
+    to: "Київ",
+    departure: "08:30",
+    duration: "5 год 20 хв"
+  },
+  {
+    id: 2,
+    number: "IC 721",
+    from: "Львів",
+    to: "Одеса",
+    departure: "10:15",
+    duration: "8 год 10 хв"
+  },
+  {
+    id: 3,
+    number: "IC 705",
+    from: "Київ",
+    to: "Харків",
+    departure: "07:00",
+    duration: "4 год 50 хв"
   }
 ];
