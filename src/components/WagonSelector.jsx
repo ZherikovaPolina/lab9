259c2141e306ab5ba4de659849017e245c9df523
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-26 17:50:47 +0300

    highlight selected wagon

--- src/components/WagonSelector.jsx
+++ src/components/WagonSelector.jsx
@@ -1,14 +1,18 @@
 import { useBooking } from "../context/BookingContext";
 
 export default function WagonSelector() {
-  const { setSelectedWagon } = useBooking();
+  const { selectedWagon, setSelectedWagon } = useBooking();
 
   const wagons = [1, 2, 3, 4];
 
   return (
     <div>
       {wagons.map((w) => (
-        <button key={w} onClick={() => setSelectedWagon(w)}>
+        <button
+          key={w}
+          onClick={() => setSelectedWagon(w)}
+          className={selectedWagon === w ? "active" : ""}
+        >
           Вагон {w}
         </button>
       ))}
