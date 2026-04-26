c09d6a51b6be5296563903f9b14434fa6dafbfcc
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-26 17:55:25 +0300

    add selected wagon state to booking context

--- src/context/BookingContext.jsx
+++ src/context/BookingContext.jsx
@@ -4,12 +4,15 @@ const BookingContext = createContext();
 
 export function BookingProvider({ children }) {
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [selectedWagon, setSelectedWagon] = useState(1);
 
   return (
     <BookingContext.Provider
       value={{
         selectedSeats,
         setSelectedSeats,
+        selectedWagon,
+        setSelectedWagon,
       }}
     >
       {children}
