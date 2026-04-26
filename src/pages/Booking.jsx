931c3b68056170bf5a74d87fe4f2d5511d22eebf
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-26 21:54:48 +0300

    passed trainId through booking page to properly connect routing with booking components

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,15 +1,26 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useBooking } from "../context/BookingContext";
+
 import WagonSelector from "../components/WagonSelector";
 import SeatMap from "../components/SeatMap";
 import BookingForm from "../components/BookingForm";
 
 export default function Booking() {
+  const { trainId } = useParams();
+  const { setSelectedSeats } = useBooking();
+
+  useEffect(() => {
+    setSelectedSeats([]);
+  }, [trainId]);
+
   return (
     <div className="container">
       <h2>Бронювання місць</h2>
 
       <WagonSelector />
-      <SeatMap />
-      <BookingForm />
+      <SeatMap trainId={trainId} />
+      <BookingForm trainId={trainId} />
     </div>
   );
 }
