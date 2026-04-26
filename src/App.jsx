54da983841f14bb286afb108a5fc5e2c6d60a28c
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-26 18:03:24 +0300

    add Booking page way

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BookingProvider } from "./context/BookingContext";
 import Home from "./pages/Home";
+import Booking from "./pages/Booking";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/booking/:trainId" element={<div>Booking Page</div>} />
-      </Routes>
-    </BrowserRouter>
+    <BookingProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking/:trainId" element={<Booking />} />
+        </Routes>
+      </BrowserRouter>
+    </BookingProvider>
   );
 }
