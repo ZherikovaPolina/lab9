60dfa829b09235bd6009ae7ff0d34577200ea7c8
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-26 17:43:46 +0300

    add toast notification after successful booking

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useBooking } from "../context/BookingContext";
 import { saveBooking } from "../services/BookingService";
+import Toast from "./Toast";
 
 export default function BookingForm() {
   const { selectedSeats, setSelectedSeats, selectedWagon } = useBooking();
@@ -11,21 +12,23 @@ export default function BookingForm() {
     email: "",
   });
 
-  const [error, setError] = useState("");
+  const [toast, setToast] = useState(false);
 
   const handleSubmit = () => {
-    if (!form.name || !form.phone || !form.email) {
-      setError("Заповніть всі поля");
-      return;
-    }
+    if (!form.name || !form.phone || !form.email) return;
 
     saveBooking(selectedSeats, form, selectedWagon);
+
     setSelectedSeats([]);
-    setError("");
+
+    setToast(true);
+    setTimeout(() => setToast(false), 2000);
   };
 
   return (
     <div className="booking-form">
+      <Toast show={toast} message="Бронювання успішне 🎉" />
+
       <input
         placeholder="Ім’я"
         value={form.name}
@@ -43,8 +46,6 @@ export default function BookingForm() {
         value={form.email}
         onChange={(e) => setForm({ ...form, email: e.target.value })}
       />
-
-      {error && <p className="error">{error}</p>}
 
       <button onClick={handleSubmit} disabled={selectedSeats.length === 0}>
         Забронювати ({selectedSeats.length})
