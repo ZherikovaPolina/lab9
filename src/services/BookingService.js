6639db4ac3689e6150a2a2f19baf02a7f237b340
Polina Zherikova bc2025-7 <polinagerikova@gmail.com>
2026-04-29 12:31:52 +0300

    add removeBooking function using filter

--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -1,5 +1,6 @@
 export function saveBooking(seats, user, wagonId, trainId) {
   const existing = JSON.parse(localStorage.getItem("bookedSeats")) || [];
+
   const newBookings = seats.map((seat) => ({
     seat,
     wagonId,
@@ -14,4 +15,19 @@ export function saveBooking(seats, user, wagonId, trainId) {
 
 export function getBookedSeats() {
   return JSON.parse(localStorage.getItem("bookedSeats")) || [];
+}
+
+export function removeBooking(seat, wagonId, trainId) {
+  const existing = JSON.parse(localStorage.getItem("bookedSeats")) || [];
+
+  const updated = existing.filter(
+    (b) =>
+      !(
+        b.seat === seat &&
+        b.wagonId === wagonId &&
+        String(b.trainId) === String(trainId)
+      )
+  );
+
+  localStorage.setItem("bookedSeats", JSON.stringify(updated));
 }
